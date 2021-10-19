75c0102fc4ce86702e42b294fcde46578c26cbaa
Michael Neises <neisesmichael@gmail.com>
2021-10-19 05:02:34 -0500

    prevent crash on dock-start with Modvehicle

---
+++ VehicleFramework/VehicleFramework/Patches/VehicleDockingBayPatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(VehicleDockingBay))]
+    class VehicleDockingBayPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("OnTriggerEnter")]
+        public static bool OnTriggerEnterPrefix(VehicleDockingBay __instance, Collider other)
+        {
+            if(UWE.Utils.GetComponentInHierarchy<ModVehicle>(other.gameObject) != null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
