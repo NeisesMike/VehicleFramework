72f839637f3553bc1b29887e12008f85d1453ac5
Michael Neises <neisesmichael@gmail.com>
2021-11-09 23:12:17 -0600

    add dead code for use with SE patch maybe

---
+++ VehicleFramework/VehicleFramework/Patches/VehicleUpgradeConsoleInputPatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(VehicleUpgradeConsoleInput))]
+    class VehicleUpgradeConsoleInputPatcher
+    {
+        // this is slotextender compat shit
+        [HarmonyPrefix]
+        [HarmonyPatch("OpenPDA")]
+        public static void OpenPDAPrefix(VehicleUpgradeConsoleInput __instance)
+        {
+            // configure the appropriate AllSlots
+            if (__instance.GetComponentInParent<ModVehicle>() != null)
+            {
+                //ModuleBuilder.main.vehicleAllSlots = null;
+            }
+            else
+            {
+                //ModuleBuilder.main.vehicleAllSlots = null;
+            }
+        }
+    }
+}
