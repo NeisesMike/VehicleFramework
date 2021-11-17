5abbb0c88f3bb4479d5430b79cc75438e9d82180
Michael Neises <neisesmichael@gmail.com>
2021-11-16 20:33:55 -0600

    disallow saving while piloting a ModVehicle

---
+++ VehicleFramework/VehicleFramework/Patches/InGameMenuPatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace VehicleFramework.Patches
+{
+	[HarmonyPatch(typeof(IngameMenu))]
+	public static class IngameMenuPatcher
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch("GetAllowSaving")]
+		public static bool GetAllowSavingPrefix(ref bool __result)
+		{
+			bool isPilotingMV = false;
+			foreach(ModVehicle mv in VehicleManager.VehiclesInPlay)
+            {
+				if(mv.IsPlayerPiloting())
+                {
+					isPilotingMV = true;
+					break;
+                }
+            }
+			if(isPilotingMV)
+            {
+				__result = false;
+				return false;
+            }
+			return true;
+		}
+	}
+}
