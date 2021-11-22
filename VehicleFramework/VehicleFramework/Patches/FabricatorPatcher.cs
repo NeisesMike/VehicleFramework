906f2c9023a5c46abb0dd09b111025ecd867a43b
Michael Neises <neisesmichael@gmail.com>
2021-11-21 18:41:55 -0600

    add patches necessary for fab and voice pause

---
+++ VehicleFramework/VehicleFramework/Patches/FabricatorPatcher.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace VehicleFramework.Patches
+{
+
+
+	[HarmonyPatch(typeof(GhostCrafter))]
+	public static class FabricatorPatcher
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch("HasEnoughPower")]
+		public static bool HasEnoughPowerPrefix(GhostCrafter __instance, ref bool __result)
+		{
+			ModVehicle mv = __instance.GetComponentInParent<ModVehicle>();
+			if (mv is null || !GameModeUtils.RequiresPower())
+            {
+				return true;
+			}
+            PowerManager.PowerStatus goodPS = new PowerManager.PowerStatus
+            {
+                hasFuel = true,
+                isPowered = true
+            };
+            __result = mv.GetComponent<PowerManager>().EvaluatePowerStatus() == goodPS;
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(CrafterLogic))]
+	public static class CrafterLogicPatcher
+	{
+		[HarmonyPrefix]
+		[HarmonyPatch("ConsumeEnergy")]
+		public static bool ConsumeEnergyPrefix(CrafterLogic __instance, ref bool __result, PowerRelay powerRelay, float amount)
+		{
+			if(!GameModeUtils.RequiresPower())
+            {
+				// If this is Creative or something, don't even bother
+				return true;
+            }
+			if (powerRelay is null)
+            {
+				// if powerRelay was null, we must be talking about a ModVehicle
+				// so let's check for one
+				ModVehicle mv = null;
+				foreach(ModVehicle tempMV in VehicleManager.VehiclesInPlay)
+                {
+					if(tempMV.IsPlayerInside())
+                    {
+						mv = tempMV;
+						break;
+                    }
+                }
+				if(mv is null)
+                {
+					Logger.Log("ConsumeEnergyPrefix ERROR: PowerRelay was null, but we weren't in a ModVehicle.");
+					return true;
+                }
+				else
+                {
+					// we found the ModVehicle from whose fabricator we're trying to drain power
+					__result = 5 <= mv.powerMan.TrySpendEnergy(5f);
+					return false;
+				}
+            }
+			// we should never make it here... so let the base game throw an error :shrug:
+			return true;
+		}
+	}
+}
---
+++ VehicleFramework/VehicleFramework/Patches/FreezeTimePatcher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UWE;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+	[HarmonyPatch(typeof(FreezeTime))]
+	public static class FreezeTimePatcher
+	{
+		[HarmonyPostfix]
+		[HarmonyPatch("PauseSound")]
+		public static void PauseSoundPostfix(bool pause)
+		{
+			foreach (ModVehicle mv in VehicleManager.VehiclesInPlay)
+			{
+				mv.voice.PauseSpeakers(pause);
+			}
+		}
+	}
+}
