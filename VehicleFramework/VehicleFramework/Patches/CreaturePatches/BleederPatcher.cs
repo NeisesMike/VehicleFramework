047c842fd113f2a187ce3cd2887cf6bbe138e394
Michael Neises <neisesmichael@gmail.com>
2023-10-27 02:25:41 -0500

    fix bleeder attaching through modvehicles

---
+++ VehicleFramework/VehicleFramework/Patches/CreaturePatches/BleederPatcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using System.Reflection.Emit;
+
+namespace VehicleFramework.Patches.CreaturePatches
+{
+    [HarmonyPatch(typeof(AttachAndSuck))]
+    class BleederPatcher
+    {
+        /* This patch is intended to ensure bleeder's take the player's life and not the vehicle's life.
+         * It works by adding a check for whether the player is in a modvehicle before it attaches
+         * Without this, the bleeder will collide with the modvehicle, find that it has a player, but that the player isn't piloting or in a cyclops (so he's vulnerable)
+         * Then it will attach to the player but deal damage to the modvehicle
+         */
+        public static bool IsPlayerInsideModVehicle()
+        {
+            return (Player.main.GetVehicle() is ModVehicle);
+        }
+        [HarmonyPatch("OnCollisionEnter")]
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            List<CodeInstruction> newCodes = new List<CodeInstruction>(codes.Count + 2);
+            CodeInstruction myNOP = new CodeInstruction(OpCodes.Nop);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                newCodes.Add(myNOP);
+            }
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Callvirt)
+                {
+                    if (codes[i].operand.ToString().Contains("IsInSub"))
+                    {
+                        newCodes[i] = codes[i];
+                        newCodes[i + 1] = codes[i + 1];
+                        newCodes[i + 2] = CodeInstruction.Call(typeof(BleederPatcher), nameof(IsPlayerInsideModVehicle));
+                        newCodes[i + 3] = new CodeInstruction(codes[i + 1]);
+                        i += 4;
+                        continue;
+                    }
+                }
+                newCodes[i] = codes[i];
+            }
+            return newCodes.AsEnumerable();
+        }
+    }
+}
