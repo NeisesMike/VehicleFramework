59a6440d05f0a718785f1cd5d8dff197202ecb55
Michael Neises <neisesmichael@gmail.com>
2024-01-14 20:54:28 -0600

    add new admin classes

---
+++ VehicleFramework/VehicleFramework/Admin/GameObjectManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace VehicleFramework.Admin
+{
+    public interface IGameObjectManager
+    {
+        void ClearList();
+    }
+    public class GameObjectManager<T> : IGameObjectManager where T : Component
+    {
+        public static List<T> AllSuchObjects = new List<T>();
+        public static T FindNearestSuch(Vector3 target, Func<T, bool> filter=null)
+        {
+            float ComputeDistance(T thisObject)
+            {
+                try
+                {
+                    return Vector3.Distance(target, thisObject.transform.position);
+                }
+                catch
+                {
+                    return 99999;
+                }
+            }
+
+            List<T> FilteredSuchObjects = AllSuchObjects.Where(x => x != null).ToList();
+            if(filter != null)
+            {
+                FilteredSuchObjects = FilteredSuchObjects.Where(x=>filter(x)).ToList();
+            }
+            return FilteredSuchObjects.OrderBy(x => ComputeDistance(x)).FirstOrDefault();
+        }
+
+        public static void Register(T cont)
+        {
+            AllSuchObjects.Add(cont);
+        }
+
+        public static void Deregister(T cont)
+        {
+            AllSuchObjects.Remove(cont);
+        }
+
+        public void ClearList()
+        {
+            AllSuchObjects.Clear();
+        }
+    }
+
+}
---
+++ VehicleFramework/VehicleFramework/Admin/GameStateWatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace VehicleFramework.Admin
+{
+    public static class GameStateWatcher
+    {
+        public static bool IsPlayerAwaked;
+        public static bool IsPlayerStarted;
+
+        public static List<IGameObjectManager> GOManagers = new List<IGameObjectManager>();
+        public static void OnResetScene(Scene scene)
+        {
+            VehicleManager.VehiclesInPlay.Clear();
+            GOManagers.ForEach(x => x.ClearList());
+            IsPlayerAwaked = false;
+            IsPlayerStarted = false;
+        }
+    }
+}
