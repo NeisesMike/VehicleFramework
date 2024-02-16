23782bc09be458562056f013722644bb44d19faf
Michael Neises <neisesmichael@gmail.com>
2024-02-15 18:28:47 -0600

    add plugin info

---
+++ VehicleFramework/VehicleFramework/PluginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleFramework
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "com.mikjaw.subnautica.vehicleframework.mod";
+        public const string PLUGIN_NAME = "VehicleFramework";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
