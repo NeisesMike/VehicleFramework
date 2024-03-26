22398fa0feb2e26eead278d17b1c6cf6782a3d89
Michael Neises <neisesmichael@gmail.com>
2024-03-26 16:12:25 -0500

    add spawn patch for rapid development

---
+++ VehicleFramework/VehicleFramework/Patches/SpawnConsoleCommandPatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(SpawnConsoleCommand))]
+    public static class SpawnConsoleCommandPatcher
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SpawnConsoleCommand.OnConsoleCommand_spawn))]
+        public static void OnConsoleCommand_spawnPostfix(SpawnConsoleCommand __instance, NotificationCenter.Notification n)
+        {
+            if (n != null && n.data != null && n.data.Count > 0)
+            {
+                string text = (string)n.data[0];
+                if (UWE.Utils.TryParseEnum<TechType>(text, out TechType techType))
+                {
+                    UWE.CoroutineHost.StartCoroutine(CheckSpawnForMVs(techType));
+                }
+            }
+        }
+        public static void FinishAnySpawningVehicles()
+        {
+            void FinishHim(ModVehicle mv)
+            {
+                mv.GetComponentInChildren<VFXConstructing>(true).constructed = 90f;
+                mv.GetComponentInChildren<VFXConstructing>(true).delay = 0f;
+            }
+            VehicleManager.VehiclesInPlay
+                .Where(x => x.GetComponentInChildren<VFXConstructing>(true) != null && x.GetComponentInChildren<VFXConstructing>(true).constructed < 100f)
+                .ForEach(x => FinishHim(x));
+        }
+        public static IEnumerator CheckSpawnForMVs(TechType tt)
+        {
+            CoroutineTask<GameObject> request = CraftData.GetPrefabForTechTypeAsync(tt, true);
+            yield return request;
+            GameObject result = request.GetResult();
+            if (result != null && result.GetComponent<ModVehicle>() != null)
+            {
+                yield return new WaitForSeconds(0.5f);
+                FinishAnySpawningVehicles();
+            }
+        }
+    }
+}
