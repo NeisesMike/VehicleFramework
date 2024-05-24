bd50ef7af368325f76661d520e4781146a713d6d
Michael Neises <neisesmichael@gmail.com>
2024-05-24 02:47:16 -0500

    Allow fabricator deconstruction

--- VehicleFramework/VehicleFramework/Patches/FabricatorPatcher.cs
+++ VehicleFramework/VehicleFramework/Patches/FabricatorPatcher.cs
@@ -27,18 +27,6 @@ namespace VehicleFramework.Patches
 			__result = mv.GetComponent<PowerManager>().EvaluatePowerStatus() == goodPS;
 			return false;
 		}
-		[HarmonyPrefix]
-		[HarmonyPatch("CanDeconstruct")]
-		public static bool CanDeconstructPrefix(GhostCrafter __instance, string reason, ref bool __result)
-		{
-			ModVehicle mv = __instance.GetComponentInParent<ModVehicle>();
-			if (mv is null)
-			{
-				return true;
-			}
-			__result = !(__instance is Fabricator);
-			return false;
-		}
 	}
 
 	[HarmonyPatch(typeof(CrafterLogic))]
