415e91cf3c284c94dbfc1a90caabb4f465cbc3ac
Michael Neises <neisesmichael@gmail.com>
2024-06-19 23:20:42 -0500

    Drone station captures cycle inputs

---
+++ VehicleFramework/VehicleFramework/Patches/uGUI_QuickSlotsPatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework
+{
+    [HarmonyPatch(typeof(uGUI_QuickSlots))]
+    public class uGUI_QuickSlotsPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(uGUI_QuickSlots.HandleInput))]
+        public static bool HandleInputPrefix()
+        {
+            Targeting.GetTarget(Player.main.gameObject, 6f, out GameObject target, out float _);
+            if(target == null)
+            {
+                return true;
+            }
+            if (target.name.Contains("DroneStation") || target.name.Contains("submarine_Picture_Frame_button"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
