ea5f30f79c62585c1e3864733e0e67a1d5095113
Michael Neises <neisesmichael@gmail.com>
2024-06-19 22:33:00 -0500

    Add visual noise to drone cameras

---
+++ VehicleFramework/VehicleFramework/Patches/MapRoomCameraScreenFXControllerPatcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using VehicleFramework.VehicleTypes;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(MapRoomCameraScreenFXController))]
+    class MapRoomCameraScreenFXControllerPatcher
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(MapRoomCameraScreenFXController.OnPreRender))]
+        public static void OnPreRenderPostfix(MapRoomCameraScreenFXController __instance)
+        {
+            Drone drone = Drone.mountedDrone;
+            if(drone == null)
+            {
+                return;
+            }
+            float noise;
+            float distance = Vector3.Distance(Player.main.transform.position, drone.transform.position);
+            const float threshold = 350;
+            if(distance < threshold)
+            {
+                noise = 0;
+            }
+            else
+            {
+                noise = 0.04f * ((distance - threshold) / 10f);
+            }
+            __instance.fx.noiseFactor = noise;
+        }
+    }
+}
