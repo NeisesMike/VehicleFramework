faf4a0e91a8baa632910bf47a9b82d50e99762db
Michael Neises <neisesmichael@gmail.com>
2024-06-22 17:31:29 -0500

    manage storage at cyclops console
    It only uses the Seamoth HUD. It always provides exactly 4 console buttons. Only those with corresponding storage will actually open the storage.

---
+++ VehicleFramework/VehicleFramework/Patches/CyclopsVehicleStorageTerminalManagerPatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(CyclopsVehicleStorageTerminalManager))]
+    public static class CyclopsVehicleStorageTerminalManagerPatcher
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(CyclopsVehicleStorageTerminalManager.StorageButtonClick))]
+        public static void StorageButtonClickPostfix(CyclopsVehicleStorageTerminalManager __instance, CyclopsVehicleStorageTerminalManager.VehicleStorageType type, int slotID)
+        {
+            if (__instance.dockedVehicleType == CyclopsVehicleStorageTerminalManager.DockedVehicleType.Seamoth)
+            {
+                foreach (StorageInput seamothStorageInput in __instance.currentVehicle.GetAllComponentsInChildren<StorageInput>())
+                {
+                    if (seamothStorageInput.slotID == slotID)
+                    {
+                        seamothStorageInput.OpenFromExternal();
+                        return;
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
