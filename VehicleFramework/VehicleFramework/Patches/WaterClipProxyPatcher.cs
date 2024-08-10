e3b5046d75458a3a1622e50c864fcfbde8d4c47b
Michael Neises <neisesmichael@gmail.com>
2024-08-10 12:56:42 -0500

    Fix inverted scaling issue

---
+++ VehicleFramework/VehicleFramework/Patches/WaterClipProxyPatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(WaterClipProxy))]
+    class WaterClipProxyPatcher
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(WaterClipProxy.UpdateMaterial))]
+        static void VehicleDockedPostfix(WaterClipProxy __instance)
+        {
+            ModVehicle vehicle = __instance.GetComponentInParent<ModVehicle>();
+            if (vehicle != null)
+            {
+                Vector3 oldScale = __instance.transform.lossyScale;
+                Vector3 newScale = new Vector3(
+                    oldScale.x < 0 ? -oldScale.x : oldScale.x,
+                    oldScale.y < 0 ? -oldScale.y : oldScale.y,
+                    oldScale.z < 0 ? -oldScale.z : oldScale.z
+                    );
+                __instance.clipMaterial.SetVector(ShaderPropertyID._ObjectScale, newScale);
+            }
+        }
+    }
+}
