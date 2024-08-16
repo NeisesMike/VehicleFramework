848fdc898f5ee9cc13ec284e00d95b06d4d2be02
Michael Neises <neisesmichael@gmail.com>
2024-08-16 14:31:13 -0500

    Add compat for Chameleon sub

---
+++ VehicleFramework/VehicleFramework/Patches/CompatibilityPatches/ChameleonSubPatcher.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace VehicleFramework.Patches.CompatibilityPatches
+{
+    [HarmonyPatch(typeof(ConstructorInput))]
+    public static class ChameleonSubPatcher
+    {
+        /*
+         * This patch is specifically for the Chameleon mod.
+         * It ensures ModVehicles are built normally;
+         * without it, Submarines don't finish being constructed correctly,
+         * and mostly obviously, they don't get fabricators.
+         * 
+         * The purpose of this "empty" patch is to bump the transpilation chain on this method.
+         * I don't know why it fixes the problem.
+         * I don't even know why the original transpiler is a problem.
+         */
+        [HarmonyPatch(nameof(ConstructorInput.OnCraftingBeginAsync)), HarmonyPatch(MethodType.Enumerator), HarmonyPostfix]
+        public static void OnCraftingBeginAsyncPostfix()
+        {
+        }
+    }
+}
