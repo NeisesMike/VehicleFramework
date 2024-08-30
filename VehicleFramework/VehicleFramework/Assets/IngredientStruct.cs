19458617c2c961d13bacf821f53aca4e429c8fca
Michael Neises <neisesmichael@gmail.com>
2024-08-30 03:50:20 -0500

    Create IngredientStruct.cs

---
+++ VehicleFramework/VehicleFramework/Assets/IngredientStruct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleFramework.Assets
+{
+    public struct Ingredient
+    {
+        public TechType techType;
+        public int count;
+        public Ingredient(TechType inTechType, int inCount)
+        {
+            techType = inTechType;
+            count = inCount;
+        }
+        public CraftData.Ingredient Get()
+        {
+            return new CraftData.Ingredient(techType, count);
+        }
+    }
+}
