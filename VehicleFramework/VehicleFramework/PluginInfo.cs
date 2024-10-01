b72a288519bafddd7387b13e365a820977c6832c
Michael Neises <neisesmichael@gmail.com>
2024-10-01 16:39:24 -0500

    Update PluginInfo.cs

--- VehicleFramework/VehicleFramework/PluginInfo.cs
+++ VehicleFramework/VehicleFramework/PluginInfo.cs
@@ -10,6 +10,6 @@ namespace VehicleFramework
     {
         public const string PLUGIN_GUID = "com.mikjaw.subnautica.vehicleframework.mod";
         public const string PLUGIN_NAME = "VehicleFramework";
-        public const string PLUGIN_VERSION = "1.3.3";
+        public const string PLUGIN_VERSION = "1.3.5";
     }
 }
