9aebfd1d29f87c97a724b54e5224839c2d3b570b
Michael Neises <neisesmichael@gmail.com>
2024-10-01 16:39:18 -0500

    Add extension method for getting current ModVehicle

---
+++ VehicleFramework/VehicleFramework/Admin/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleFramework
+{
+    public static class ExtensionMethods
+    {
+        public static ModVehicle GetModVehicle(this Player player)
+        {
+            return player.GetVehicle() as ModVehicle
+                ?? player.currentSub.GetComponent<ModVehicle>();
+        }
+    }
+}
