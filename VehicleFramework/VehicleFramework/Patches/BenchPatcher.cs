97afb6a6801630e567765da2811d5cdf3b400628
Michael Neises <neisesmichael@gmail.com>
2024-10-31 22:31:59 -0500

    Enable player to look up and down in a chair in an MV

---
+++ VehicleFramework/VehicleFramework/Patches/BenchPatcher.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(Bench))]
+    public class BenchPatcher
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Bench.EnterSittingMode))]
+        public static void BenchEnterSittingModePostfix()
+        {
+            if(Player.main.GetModVehicle() != null)
+            {
+                Player.main.GetModVehicle().controlSheme = Vehicle.ControlSheme.Mech;
+            }
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Bench.ExitSittingMode))]
+        public static void BenchExitSittingModePostfix()
+        {
+            if (Player.main.GetModVehicle() != null)
+            {
+                Player.main.GetModVehicle().controlSheme = (Vehicle.ControlSheme)12;
+            }
+        }
+    }
+}
