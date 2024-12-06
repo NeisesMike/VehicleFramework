2833f21134ebbf8293877002ed086193fe48b733
Michael Neises <neisesmichael@gmail.com>
2024-12-06 15:02:47 -0600

    Add color changing hooks to vehicle upgrade bay

--- VehicleFramework/VehicleFramework/Patches/SubNamePatcher.cs
+++ VehicleFramework/VehicleFramework/Patches/SubNamePatcher.cs
@@ -16,7 +16,6 @@ namespace VehicleFramework.Patches
                 SetSubNameDecals(mv);
             }
         }
-
         private static void SetSubNameDecals(ModVehicle mv)
         {
             foreach (var tmprougui in mv.SubNameDecals)
@@ -24,6 +23,50 @@ namespace VehicleFramework.Patches
                 tmprougui.font = Nautilus.Utility.FontUtils.Aller_Rg;
                 tmprougui.text = mv.subName.GetName();
             }
+        }
+
+        private static void SetSubNameDecalsWithColor(ModVehicle mv, Vector3 hsb, Color color)
+        {
+            SetSubNameDecals(mv);
+            foreach (var tmprougui in mv.SubNameDecals)
+            {
+                tmprougui.color = color;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(SubName.SetColor))]
+        public static void SubNameSetColorPostfix(SubName __instance, int index, Vector3 hsb, Color color)
+        {
+            ModVehicle mv = __instance.GetComponent<ModVehicle>();
+            if (mv == null)
+            {
+                return;
+            }
+            if (index == 0)
+            {
+                mv.SetBaseColor(hsb, color);
+            }
+            else if (index == 1)
+            {
+                if (mv.SubNameDecals != null)
+                {
+                    SetSubNameDecalsWithColor(mv, hsb, color);
+                }
+            }
+            else if (index == 2)
+            {
+                mv.SetInteriorColor(hsb, color);
+            }
+            else if (index == 3)
+            {
+                mv.SetStripeColor(hsb, color);
+            }
+            else
+            {
+                Logger.Warn("SubName.SetColor Error: Tried to set the color of an index that was not known!");
+            }
+
         }
     }
 }
