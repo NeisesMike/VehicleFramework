941f3ecb0b76490b7de026414abb7fa6c08ae084
Michael Neises <neisesmichael@gmail.com>
2025-01-12 15:18:38 -0600

    Update PluginInfo.cs

--- VehicleFramework/VehicleFramework/PluginInfo.cs
+++ VehicleFramework/VehicleFramework/PluginInfo.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace VehicleFramework
 {
     public static class PluginInfo
     {
         public const string PLUGIN_GUID = "com.mikjaw.subnautica.vehicleframework.mod";
         public const string PLUGIN_NAME = "VehicleFramework";
-        public const string PLUGIN_VERSION = "1.4.12";
+        public const string PLUGIN_VERSION = "1.4.15";
     }
 }
