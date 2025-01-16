c3b8b73fc8084405171b410f20e9189dd2674bd3
Michael Neises <neisesmichael@gmail.com>
2025-01-15 19:08:47 -0600

    Replace Bench prefix with Transpilers

--- VehicleFramework/VehicleFramework/Patches/BenchPatcher.cs
+++ VehicleFramework/VehicleFramework/Patches/BenchPatcher.cs
@@ -1,32 +1,17 @@
 ﻿using HarmonyLib;
 using UnityEngine;
+using System.Reflection.Emit;
+using System.Collections.Generic;
+using System;
 
 namespace VehicleFramework.Patches
 {
+    // The transpilers here replace all calls to Bench.ExitSittingMode with Bench.MaybeExitSittingMode,
+    // which always calls Bench.ExitSittingMode if a drone isn't being piloted,
+    // and still calls it if the player died or just became underwater.
     [HarmonyPatch(typeof(Bench))]
     public class BenchPatcher
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(Bench.ExitSittingMode))]
-        public static bool BenchExitSittingModePrefix(Player player, bool skipCinematics)
-        {
-            if(VehicleTypes.Drone.mountedDrone != null)
-            {
-                if (skipCinematics || player.isUnderwater.value)
-                {
-                    if (player.mode == Player.Mode.LockedPiloting)
-                    {
-                        VehicleTypes.Drone.mountedDrone.DeselectSlots();
-                    }
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
         [HarmonyPostfix]
         [HarmonyPatch(nameof(Bench.EnterSittingMode))]
         public static void BenchEnterSittingModePostfix()
@@ -38,12 +23,75 @@ namespace VehicleFramework.Patches
         }
         [HarmonyPostfix]
         [HarmonyPatch(nameof(Bench.ExitSittingMode))]
-        public static void BenchExitSittingModePostfix()
+        public static void BenchExitSittingModePostfix(Bench __instance, Player player, bool skipCinematics)
         {
             if (Player.main.GetModVehicle() != null)
             {
                 Player.main.GetModVehicle().controlSheme = (Vehicle.ControlSheme)12;
             }
         }
+
+        public static IEnumerable<CodeInstruction> ReplaceExitSittingMode(IEnumerable<CodeInstruction> instructions)
+        {
+            CodeMatch ExitSittingModeMatch = new CodeMatch(i => i.opcode == OpCodes.Call && i.operand.ToString().Contains("ExitSittingMode"));
+
+            var newInstructions = new CodeMatcher(instructions)
+                .MatchStartForward(ExitSittingModeMatch)
+                .RemoveInstruction()
+                .Insert(Transpilers.EmitDelegate<Action<Bench, Player, bool>>(MaybeExitSittingMode));
+
+            return newInstructions.InstructionEnumeration();
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Bench.OnUpdate))]
+        public static IEnumerable<CodeInstruction> OnUpdateTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return ReplaceExitSittingMode(instructions);
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Bench.OnPlayerDeath))]
+        public static IEnumerable<CodeInstruction> OnPlayerDeathTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return ReplaceExitSittingMode(instructions);
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Bench.CheckIfUnderwater))]
+        public static IEnumerable<CodeInstruction> CheckIfUnderwaterTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return ReplaceExitSittingMode(instructions);
+        }
+
+        public static void MaybeExitSittingMode(Bench thisBench, Player player, bool skipCinematics)
+        {
+            // If the player is piloting a drone,
+            // consume the "exit" input to leave the drone,
+            // but do not, at that time, exit the bench.
+            // If instead the player died or is underwater,
+            // leave the drone AND exit the bench.
+            if (VehicleTypes.Drone.mountedDrone != null)
+            {
+                if (skipCinematics || player.isUnderwater.value) // these are true for OnDeath and OnUnderwater, respectively
+                {
+                    if (player.mode == Player.Mode.LockedPiloting)
+                    {
+                        // drop the drone controls
+                        VehicleTypes.Drone.mountedDrone.DeselectSlots();
+                    }
+                    // Exit the bench normally
+                    thisBench.ExitSittingMode(player, skipCinematics);
+                }
+                // so in the case of OnUpdate (which is just when the button is pressed)
+                // do nothing!
+            }
+            else
+            {
+                // do nothing different if we aren't piloting a drone
+                thisBench.ExitSittingMode(player, skipCinematics);
+            }
+        }
+
     }
 }
