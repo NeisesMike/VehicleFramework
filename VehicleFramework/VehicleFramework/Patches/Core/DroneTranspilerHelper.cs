12439c4999dde137bfea77e038f98f260a2d34fc
Michael Neises <neisesmichael@gmail.com>
2025-01-16 00:47:10 -0600

    Add new transpiler helper

---
+++ VehicleFramework/VehicleFramework/Patches/Core/DroneTranspilerHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using System.Reflection.Emit;
+
+namespace VehicleFramework.Patches
+{
+    public static class DroneTranspilerHelper
+    {
+        public static bool IsPlayerUsingDrone()
+        {
+            return VehicleTypes.Drone.mountedDrone != null;
+        }
+
+        public static IEnumerable<CodeInstruction> SkipForDrones(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var newInstructions = new CodeMatcher(instructions, generator)
+                .Start()
+                .CreateLabel(out Label labelAfterRet)
+                .InsertAndAdvance(Transpilers.EmitDelegate<Func<bool>>(IsPlayerUsingDrone))
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Brfalse_S, labelAfterRet))
+                .InsertAndAdvance(new CodeInstruction(OpCodes.Ret));
+
+            return newInstructions.InstructionEnumeration();
+        }
+    }
+}
