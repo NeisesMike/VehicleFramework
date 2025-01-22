819cec00e471a0abacac3ef7a58648f32e9d8ed1
Michael Neises <neisesmichael@gmail.com>
2025-01-22 12:26:57 -0600

    Fix possible null dereference

--- VehicleFramework/VehicleFramework/Admin/VehicleManager.cs
+++ VehicleFramework/VehicleFramework/Admin/VehicleManager.cs
@@ -43,7 +43,7 @@ namespace VehicleFramework
             {
                 VehiclesInPlay.Add(mv);
                 Logger.Log("Enrolled the " + mv.name + " : " + mv.GetName() + " : " + mv.subName);
-                if (mv.GetComponent<VFXConstructing>().constructed > 3f)
+                if (mv.GetComponent<VFXConstructing>() == null || mv.GetComponent<VFXConstructing>().constructed > 3f)
                 {
                     UWE.CoroutineHost.StartCoroutine(LoadVehicle(mv)); // I wish I knew a good way to optionally NOT do this if this sub is being constructed rn
                 }
