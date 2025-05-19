165d9a462eee8892f40683f81867aafe730e7075
Michael Neises <neisesmichael@gmail.com>
2025-05-19 15:38:36 -0500

    move CompatChecker to Logger API

--- VehicleFramework/VehicleFramework/CompatChecker.cs
+++ VehicleFramework/VehicleFramework/CompatChecker.cs
@@ -10,52 +10,21 @@ namespace VehicleFramework
     {
         internal static void CheckAll()
         {
-            IEnumerator CheckAllInTime()
-            {
-                yield return new WaitUntil(() => ErrorMessage.main != null);
-                yield return new WaitForSeconds(1);
-                yield return new WaitUntil(() => ErrorMessage.main != null);
-                CheckAllSoon();
-                UWE.CoroutineHost.StartCoroutine(MakeAlerts());
-            }
-            UWE.CoroutineHost.StartCoroutine(CheckAllInTime());
-        }
-
-        #region private_utilities
-        private static readonly List<string> Notifications = new List<string>();
-        private static void ShowError(string message)
-        {
-            string result = $"<color=#FF0000>Vehicle Framework Error: </color><color=#FFFF00>{message}</color>";
-            Notifications.Add(result);
-            Logger.Error(message);
-        }
-        private static void ShowWarning(string message)
-        {
-            string result = $"<color=#FF0000>Vehicle Framework Warning: </color><color=#FFFF00>{message}</color>";
-            Notifications.Add(result);
-            Logger.Error(message);
-        }
-        private static IEnumerator MakeAlerts()
-        {
-            ErrorMessage eMain = ErrorMessage.main;
-            float messageDuration = eMain.timeFlyIn + eMain.timeDelay + eMain.timeFadeOut + eMain.timeInvisible + 0.1f;
-            while(Player.main == null)
-            {
-                foreach(string notif in Notifications)
-                {
-                    ErrorMessage.AddMessage(notif);
-                    yield return new WaitForSeconds(1);
-                }
-                yield return new WaitForSeconds(messageDuration);
-            }
-        }
-        private static void CheckAllSoon()
-        {
             CheckForNautilusUpdate();
             CheckForBepInExPackUpdate();
             CheckForFlareDurationIndicator();
             CheckForBuildingTweaks();
             CheckForVanillaExpanded();
+        }
+
+        #region private_utilities
+        private static void ShowError(string message)
+        {
+            Logger.LoopMainMenuError(message, "Vehicle Framework");
+        }
+        private static void ShowWarning(string message)
+        {
+            Logger.LoopMainMenuWarning(message, "Vehicle Framework");
         }
         #endregion
 
@@ -101,7 +70,7 @@ namespace VehicleFramework
             const string vanillaExpandedGUID = "VanillaExpanded";
             if (Chainloader.PluginInfos.ContainsKey(vanillaExpandedGUID))
             {
-                ShowError("Not compatible with Vanilla Expanded!");
+                ShowError("Some vehicles not compatible with Vanilla Expanded!");
                 Logger.Log("Vanilla Expanded has a patch on UniqueIdentifier.Awake that throws an error (dereferences null) during many Vehicle Framework setup methods. If you choose to continue, some vehicles, buildables, and fragments may simply not appear.");
             }
         }
