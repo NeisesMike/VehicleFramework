e732146c31f64989ceaa81c041bb8eedcdaf2b25
Michael Neises <neisesmichael@gmail.com>
2025-05-30 16:37:52 -0500

    Move file. Add more leviathan names to check

---
+++ VehicleFramework/VehicleFramework/Patches/CreaturePatcher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using HarmonyLib;
+
+// PURPOSE: ModVehicles have an awareness of nearby leviathans
+// VALUE: Moderate. Allows for some very cool/scary moments.
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(Creature))]
+    [HarmonyPatch(nameof(Creature.ChooseBestAction))]
+    class CreaturePatcher
+    {
+        private static List<string> LeviathanNames = new List<string>
+        {
+            "GhostLeviathan",
+            "ReaperLeviathan",
+            "SeaDragon",
+            "BlazaLeviathan",
+            "AbyssalBlaza",
+            "Bloop",
+            "DeepBloop",
+            "GrandBloop",
+            "AncientBloop",
+            "GulperLeviathan",
+            "GulperLeviathanJuvenile",
+            "GargantuanVoid",
+            "GargantuanJuvenile",
+            "AbyssalBlaza",
+            "AnglerFish",
+        };
+
+        [HarmonyPostfix]
+        public static void Postfix(Creature __instance)
+        {
+            if (!(Player.main.GetVehicle() is ModVehicle mv)) return;
+
+            if (Vector3.Distance(Player.main.transform.position, __instance.transform.position) > 150) return;
+
+            // react to nearby dangerous leviathans
+            if (LeviathanNames.Where(x => __instance.name.Contains(x)).Any())
+            {
+                foreach (var component in Player.main.GetVehicle().GetComponentsInChildren<IVehicleStatusListener>())
+                {
+                    component.OnNearbyLeviathan();
+                }
+            }
+        }
+    }
+}
