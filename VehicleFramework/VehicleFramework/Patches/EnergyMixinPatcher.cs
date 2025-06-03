3daf60ff761337537a93fed6ee342c7ed5a335ef
Michael Neises <neisesmichael@gmail.com>
2025-06-03 18:06:10 -0500

    Fix some exit-time errors
    The game-unload errors could possibly cause memory leaks? I don't know enough to say for sure.

---
+++ VehicleFramework/VehicleFramework/Patches/EnergyMixinPatcher.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+
+// PURPOSE: Avoid an error at game-exit (LargeWorldStreamer.UnloadGlobalRoot) whereby this method is called with a null InventoryItem
+// VALUE: Moderate. Important to make sure things continue to unload correctly (memory leak?)
+
+namespace VehicleFramework.Patches
+{
+    [HarmonyPatch(typeof(EnergyMixin))]
+    public class EnergyMixinPatcher
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(EnergyMixin.NotifyHasBattery))]
+        public static bool EnergyMixinNotifyHasBatteryHarmonyPrefix(EnergyMixin __instance, InventoryItem item)
+        {
+            if (__instance.batteryModels == null) return false;
+            if (__instance.batteryModels.Length > 0)
+            {
+                if (item == null || item.item == null)
+                {
+                    if(__instance.controlledObjects != null)
+                    {
+                        __instance.controlledObjects.ForEach(x => x.SetActive(false));
+                    }
+                    if (__instance.batteryModels != null)
+                    {
+                        __instance.batteryModels.ForEach(x => x.model.SetActive(false));
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
