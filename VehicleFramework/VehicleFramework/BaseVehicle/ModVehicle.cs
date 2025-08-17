8c88cec6855f8c6559d0a14020b2471884981d1e
Michael Neises <neisesmichael@gmail.com>
2025-08-17 16:01:27 -0500

    init these files

---
+++ VehicleFramework/VehicleFramework/BaseVehicle/ModVehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleFramework.Composition;
+using VehicleFramework.Configuration;
+using VehicleFramework;
+using VehicleFramework.Admin;
+using VehicleFramework.VehicleComponents;
+using UnityEngine;
+
+namespace VehicleFramework.BaseVehicle
+{
+    internal abstract partial class ModVehicle
+    {
+    }
+}
