5be7b676643049a01380c8d291d15b9d567bbd68
Michael Neises <neisesmichael@gmail.com>
2025-10-22 18:18:01 -0500

    Fix Bleeder Transpiler (more polite)

--- VehicleFramework/VehicleFramework/Patches/CreaturePatches/BleederPatcher.cs
+++ VehicleFramework/VehicleFramework/Patches/CreaturePatches/BleederPatcher.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using HarmonyLib;
+using System.Reflection;
 using System.Reflection.Emit;
 
 // PURPOSE: ensure bleeders deal damage in an intuitive way
@@ -20,37 +22,26 @@ namespace VehicleFramework.Patches.CreaturePatches
         {
             return (Player.main.GetVehicle() is ModVehicle);
         }
+
+        [HarmonyTranspiler]
         [HarmonyPatch(nameof(AttachAndSuck.OnCollisionEnter))]
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        public static IEnumerable<CodeInstruction> AttachAndSuckOnCollisionEnterTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
-            // Add an extra check to see if the player is in a mod vehicle.
-            // Add it to the existing checks by way of LOGICAL OR.
-            List<CodeInstruction> codes = new(instructions);
-            List<CodeInstruction> newCodes = new(codes.Count + 4);
-            CodeInstruction myNOP = new(OpCodes.Nop);
-            for (int i = 0; i < codes.Count; i++)
-            {
-                newCodes.Add(myNOP);
-            }
-            int j = 0; // newCodes index
-            for (int i = 0; i < codes.Count; i++)
-            {
-                if (codes[i].opcode == OpCodes.Callvirt)
-                {
-                    if (codes[i].operand.ToString().Contains("IsInSub"))
-                    {
-                        newCodes[j] = codes[i];
-                        newCodes[j + 1] = codes[i + 1];
-                        newCodes[j + 2] = CodeInstruction.Call(typeof(BleederPatcher), nameof(IsPlayerInsideModVehicle));
-                        newCodes[j + 3] = new(codes[i + 1]);
-                        j += 3;
-                        i += 1;
-                        continue;
-                    }
-                }
-                newCodes[j] = codes[i];
-            }
-            return newCodes.AsEnumerable();
+            var isInSub = AccessTools.Method(typeof(Player), "IsInSub");
+            CodeMatch IsInSubMatch = new(i => i.opcode == OpCodes.Callvirt && i.operand as MethodInfo == isInSub);
+
+            var matcher = new CodeMatcher(instructions, generator)
+                .MatchForward(true, IsInSubMatch)
+                .Advance(1); // Move to the brtrue
+
+            var originalBrTrue = matcher.Instruction;
+            var clonedBrTrue = originalBrTrue.Clone();
+
+            matcher.Advance(1)
+                .InsertAndAdvance(Transpilers.EmitDelegate<Func<bool>>(IsPlayerInsideModVehicle))
+                .Insert(clonedBrTrue);
+
+            return matcher.InstructionEnumeration();
         }
     }
 }
