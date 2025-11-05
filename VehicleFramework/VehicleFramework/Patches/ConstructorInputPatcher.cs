f6c65a0209c5931d0f70eb7cec95df3168050160
Michael Neises <neisesmichael@gmail.com>
2025-11-05 13:25:04 -0600

    Finish updating ConstructorInputPatcher
    Previously, I changed this from a transpiler into a prefix. But the prefix I added was identical to the original method. This commit restores the intended functionality.

--- VehicleFramework/VehicleFramework/Patches/ConstructorInputPatcher.cs
+++ VehicleFramework/VehicleFramework/Patches/ConstructorInputPatcher.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using HarmonyLib;
-using UnityEngine;
-using System.Collections.Generic;
+﻿using HarmonyLib;
 using System.Linq;
+using UnityEngine;
 
 // PURPOSE: Allow ModVehicles to be built basically anywhere (don't "need deeper water" unnecessarily)
 // VALUE: High.
+
 
 namespace VehicleFramework.Patches
 {
@@ -18,16 +17,12 @@ namespace VehicleFramework.Patches
         {
             if (!Admin.VehicleManager.vehicleTypes.Select(x => x.techType).Contains(techType))
             {
+                // Only do something for ModVehicles
                 return true;
             }
             Vector3 zero = Vector3.zero;
             Quaternion identity = Quaternion.identity;
             __instance.GetCraftTransform(techType, ref zero, ref identity);
-            if (techType != TechType.Seamoth && techType != TechType.Exosuit && !ReturnValidCraftingPositionSpecial(__instance, zero))
-            {
-                __instance.invalidNotification.Play();
-                return false;
-            }
             if (!CrafterLogic.ConsumeResources(techType))
             {
                 return false;
@@ -41,15 +36,6 @@ namespace VehicleFramework.Patches
             }
 
             return false;
-        }
-
-        public static bool ReturnValidCraftingPositionSpecial(ConstructorInput instance, Vector3 pollPosition)
-        {
-            float num = Mathf.Clamp01((pollPosition.x + 2048f) / 4096f);
-            float num2 = Mathf.Clamp01((pollPosition.z + 2048f) / 4096f);
-            int x = (int)(num * (float)instance.validCraftPositionMap.width);
-            int y = (int)(num2 * (float)instance.validCraftPositionMap.height);
-            return instance.validCraftPositionMap.GetPixel(x, y).g > 0.5f;
         }
     }
 }
